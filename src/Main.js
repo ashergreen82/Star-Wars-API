d36e153beff2db87d4fc49f6e5622c980418be4a
Ashergreen82 <greena@rogers.com>
2023-01-16 06:51:54 -0500

    Setting up the framework

---
+++ src/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useRef, useEffect, useState } from 'react';
+// import './Expense.css';
+// import ExpenseTable from "./ExpenseTable";
+const swapi = "https://swapi.dev/api/"
+
+export default function Expense() {
+
+}
