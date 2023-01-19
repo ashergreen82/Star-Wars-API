bbd5f7872bf77d05cebd9d5d2169c61a114f9764
Ashergreen82 <greena@rogers.com>
2023-01-19 07:03:07 -0500

    started setting up the search function

--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,7 @@ const swapi = "https://swapi.dev/api/"
 
 export default function StarWars() {
     const [input, setInput] = useState("");
+    const [results, setResults] = useState();
 
     axios.get(swapi)
         .then(response => {
@@ -24,8 +25,26 @@ export default function StarWars() {
         // console.log("This is what you would like to know about the Star Wars Universe: ", { input })
     }
 
+    function getInformation(informationToGet) {
+        console.log("getInformation function executed")
+        axios.get(informationToGet)
+            .then(response => {
+                console.log("getInformation function results: ", response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     function executeSearch() {
-        console.log("Value entered =", { input })
+        // To search for something on SWAPI: https://swapi.dev/api/people/?search=r2
+        const informationToGet = "https://swapi.dev/api/people/?search=" + input
+        results = getInformation(informationToGet);
+        // const results = axios.get(informationToGet)
+        console.log("Value entered =", input);
+        console.log("Value to be searched: ", informationToGet);
+        console.log("Results object: ", results);
+        // console.log("value results: ", results);
     }
 
     return (
@@ -40,7 +59,7 @@ export default function StarWars() {
                     className="form-control"
                     value={input}
                     onChange={handleChange}
-                    placeholder="What would you like to know about the Star Wars Universe?"
+                    placeholder="Which Star Wars character are you interested in learning about?"
                     required
                 />
                 <button type="button" class="btn btn-primary mt-3" onClick={executeSearch}>Search</button>
